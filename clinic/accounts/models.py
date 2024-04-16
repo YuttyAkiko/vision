e43835de0a2f588b5fc92693913220fb703dca41
senai <senai@mail.com>
2024-04-15 22:00:54 -0300

    trabalhando nas views

--- clinic/accounts/models.py
+++ clinic/accounts/models.py
@@ -2,11 +2,10 @@ import re
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, UserManager
+    AbstractBaseUser, PermissionsMixin, UserManager, User
 )
-from django.views.decorators.http import require_POST
 
-class Criar_Usuario(AbstractBaseUser, PermissionsMixin): 
+class User(AbstractBaseUser, PermissionsMixin): 
     user_id     =   models.AutoField(primary_key=True)
     username = models.CharField('Usuário', max_length=30, unique=True, validators=[
             validators.RegexValidator(
@@ -19,26 +18,9 @@ class Criar_Usuario(AbstractBaseUser, PermissionsMixin):
         ], help_text='Um nome curto que será usado'+
                     ' para identificá-lo de forma única..'
     )
-
-    # # Lista de generos
-    # GENERO_CHOICES = (
-    #     ('F', 'Feminino')
-    #     ('M', 'Masculino')
-    #     ('N', 'Nenhuma das opções.')
-    # )
-
-    nome = models.CharField(max_length=30)
-    sobrenome = models.CharField(max_length=50)
-    genero = models.CharField(max_length=30, choices=GENERO_CHOICES)
-    cpf = models.IntegerField(max_length=15, unique=True)
-    nasc = models.DateField(auto_now=False, auto_now_add=False)
-    tel = models.IntegerField()
-    endereco = models.CharField()
-    bairro = models.CharField()
-    cidade = models.CharField()
-    email = models.CharField()
-    convenio = models.ForeignKey('Convenio', on_delete=models.CASCADE)
-    # Define usuario como ativo
+    email = models.EmailField(max_length=100)
+    senha = models.CharField(max_length=16, unique=True)
+    is_staff = models.BooleanField('Equipe', default=False)
     is_active = models.BooleanField('Ativo', default=True)
 
 # Criando um gerenciador de usuario para cada user criado
