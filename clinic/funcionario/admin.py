5e864052777bfdbc90e7a338b4d340bb8dbbaf0d
senai <senai@mail.com>
2024-04-17 21:54:29 -0300

    feature: registrado os models no admin.py

--- clinic/funcionario/admin.py
+++ clinic/funcionario/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Funcionario, Cargo, Medico, Especialidade
 
-# Register your models here.
+admin.site.register(Funcionario)
+admin.site.register(Cargo)
+admin.site.register(Medico)
+admin.site.register(Especialidade)
