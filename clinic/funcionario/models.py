c4f10f587519e57db2b25f4fd22f77af72f69e12
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-04-22 19:32:43 -0300

    refact: limpando codigo do models.py

--- clinic/funcionario/models.py
+++ clinic/funcionario/models.py
@@ -43,31 +43,4 @@ class Especialidade(models.Model):
     tipo_especialidade = models.CharField(max_length=30)
     valor_consulta = models.DecimalField(max_digits=5, decimal_places=2)
 
-# class Consulta(models.Model):
-#     # id_consulta = models.AutoField(primary_key=True, unique=True)
-#     data_cons = models.DateField(auto_now=False, auto_now_add=False)
-#     hora_cons = models.TimeField()
-#     # id_paciente = models.ForeignKey('Paciente') # Relacionamento Um p/ Muitos
-#     id_medico = models.ManyToManyField('Medico', related_name='Consulta') # Relacionamento Muitos p/ Muitos
-#     TIPOS_PAGAMENTO = (
-#         ('Cartão','Cartão'),
-#         ('Dinheiro','Dinheiro'),
-#         ('Pix','Pix')
-#     )
-#     tipo_pag_cons = models.CharField(max_length=3, choices=TIPOS_PAGAMENTO)
-#     STATUS_PAGAMENTO = (
-#         ('Pago','Pago'),
-#         ('Pendente', 'Pendente')
-#     )
-#     status_pag_cons = models.CharField(max_length=2, choices=STATUS_PAGAMENTO) # Select "Yes" ou "No" para o status de pagamento.
-#     STATUS_CONSULTA = (
-#         ('Concluída', 'Concluída'),
-#         ('Cancelada', 'Cancelada'),
-#         ('Pendente', 'Pendente'),
-#         ('Remarcada', 'Remarcada'),
-#     )
-#     status_cons = models.CharField(max_length=4, choices=STATUS_CONSULTA)
-#     motivo = models.CharField(max_length=199)
-#     sintomas = models.TextField(max_length=500, null=True)
-#     observacoes = models.TextField(max_length=500, null=True)
-#     laudo = models.TextField(max_length=500, null=True) 
+
