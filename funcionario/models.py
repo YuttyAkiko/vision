058122e30879e04af8c4495cb709399db7eecde0
Yutty Akiko <yutty.akiko@gmail.com>
2024-04-26 19:00:28 -0300

    fix: Testando as models e banco de dados

--- funcionario/models.py
+++ funcionario/models.py
@@ -2,6 +2,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Cargo(models.Model):
+    TIPOS_CARGO = (
+        ('Administrador(a)','Administrador(a)'),
+        ('Assistente','Assistente'),
+        ('Médico(a)','Médico(a)')
+    )
+    tipos_cargo = models.CharField(max_length=16, choices=TIPOS_CARGO)
+    nome_cargo = models.CharField(max_length=100)
+    descricao = models.TextField(max_length=500, null=True)
+    entrada = models.TimeField(null=True)
+    saida = models.TimeField(null=True)
+
+
 class Funcionario(models.Model):
     nome_func = models.CharField(max_length=30)
     sobrenome_func = models.CharField(max_length=50)
@@ -22,24 +35,12 @@ class Funcionario(models.Model):
     id_cargo = models.ForeignKey('Cargo', on_delete=models.CASCADE)
     status_cad_func = models.BooleanField()
 
-class Cargo(models.Model):
-    TIPOS_CARGO = (
-        ('Administrador(a)','Administrador(a)'),
-        ('Assistente','Assistente'),
-        ('Médico(a)','Médico(a)')
-    )
-    tipos_cargo = models.CharField(max_length=16, choices=TIPOS_CARGO)
-    nome_cargo = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=500, null=True)
-    entrada = models.TimeField(null=True)
-    saida = models.TimeField(null=True)
+class Especialidade(models.Model):
+    tipo_especialidade = models.CharField(max_length=30)
+    valor_consulta = models.DecimalField(max_digits=5, decimal_places=2)
 
 class Medico(models.Model):
     cnpj_med = models.CharField(max_length=12, unique=True, null=True)
     crm = models.CharField(max_length=10, unique=True)
     id_especialidade = models.ManyToManyField('Especialidade', related_name='Medicos') # Relacionamento Muitos p/ Muitos
     id_funcionario = models.OneToOneField('Funcionario', on_delete= models.CASCADE) # Relacionamento Um p/ Um
-
-class Especialidade(models.Model):
-    tipo_especialidade = models.CharField(max_length=30)
-    valor_consulta = models.DecimalField(max_digits=5, decimal_places=2)
