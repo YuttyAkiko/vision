617d104dbe3b5264315a44d241ff3996effe5ae1
YuttyAkiko <yutty.akiko@gmail.com>
2024-04-30 22:17:56 -0300

    docs: Acrescentando templates para o app paciente

--- vision/urls.py
+++ vision/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from accounts import views
+from paciente.views import background
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', background, name=background),
     # path('cadastrar/', views.cadastrar_usuario, name='cadastro')
 ]
 
