ce8d12cbce7d6726b04211adca2c65093ce7a5d6
YuttyAkiko <yutty.akiko@gmail.com>
2024-05-08 22:15:28 -0300

    Adicionando templates

--- vision/urls.py
+++ vision/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 # from accounts import views
-from paciente.views import PerfilPaciente
 from paciente.views import home
 
 urlpatterns = [
