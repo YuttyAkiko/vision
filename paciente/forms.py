85751868845e3bd6b90216314780edbd3f3fdd5a
YuttyAkiko <yutty.akiko@gmail.com>
2024-05-10 21:10:58 -0300

    feature: Acrescentei um form para paciente.

--- paciente/forms.py
+++ paciente/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import (Convenio, Paciente, Consulta, Receita, Exame)
 
+class PacienteForm(forms.ModelForm):
+    class Meta:
+        model = Paciente
+        fields = ('nome_pac','sobrenome_pac','genero_pac','cpf_pac','nasc_pac','tel_pac_1','tel_pac_2','cep_pac','end_pac','bairro_pac', 'cidade_pac','email_pac','id_convenio','carteirinha')
+
 class Update_Paciente_Form(forms.ModelForm):
 
     carteirinha = forms.CharField(max_length=100)
