33835ac6d7238741645601331e8c3bffe2d8fbe2
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-05-11 23:06:32 -0300

    feature: adicionado class -input-estilizado- para todas as fields do formulario de atualizar cadastro

--- paciente/forms.py
+++ paciente/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from .models import (Convenio, Paciente, Consulta, Receita, Exame)
 
-# ATUALIZAR DADOS DO PACIENTE
 class Update_Paciente_Form(forms.ModelForm):
 
     class Meta:
@@ -9,6 +8,7 @@ class Update_Paciente_Form(forms.ModelForm):
         fields = ('nome_pac','sobrenome_pac','genero_pac','cpf_pac','nasc_pac','tel_pac_1','tel_pac_2','cep_pac','end_pac','bairro_pac',
                   'cidade_pac','email_pac','id_convenio','num_carteirinha')
         
+    # adicionando readonly aos campos nao editaveis
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['nome_pac'].widget.attrs['readonly'] = True
@@ -17,10 +17,10 @@ class Update_Paciente_Form(forms.ModelForm):
         self.fields['cpf_pac'].widget.attrs['readonly'] = True
         self.fields['nasc_pac'].widget.attrs['readonly'] = True
         for field_name, field in self.fields.items():
-            if field_name not in ['genero_pac', 'id_convenio','nasc_pac','carteirinha']:
-                field.widget.attrs.update({'maxlength': '50', 'size': '60'})
+            field.widget.attrs.update({'class': 'input-estilizado','maxlength': '50', 'size': '60'})
+            
+                
 
-# EDITAR DADOS DE CONSULTA
 class Update_Consulta_Form(forms.ModelForm):
     class Meta:
         model = Consulta
