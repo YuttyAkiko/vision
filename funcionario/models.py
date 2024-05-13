81b0c0cb2e95e51aaf0d2735111383c20f3ee9d5
Caroline <aluno103.23187208caroline@gmail.com>
2024-05-13 19:37:46 -0300

    feature: adicionado verbose_name a tabela de funcionario

--- funcionario/models.py
+++ funcionario/models.py
@@ -9,51 +9,51 @@ class Cargo(models.Model):
         ('ATD','Atendente'),
         ('DTR','Médico(a)')
     )
-    tipos_cargo = models.CharField(max_length=16, choices=TIPOS_CARGO)
-    nome_cargo = models.CharField(max_length=100)
-    descricao = models.TextField(max_length=500, null=True)
-    entrada = models.TimeField(null=True)
-    saida = models.TimeField(null=True)
+    tipos_cargo = models.CharField(max_length=16, choices=TIPOS_CARGO, verbose_name="Tipo de Cargo")
+    nome_cargo = models.CharField(max_length=100, verbose_name="função")
+    descricao = models.TextField(max_length=500, null=True, verbose_name="descrição")
+    entrada = models.TimeField(null=True, verbose_name="Horário de Entrada")
+    saida = models.TimeField(null=True, verbose_name="Horário de Saída")
 
     def __str__(self):
         return self.nome_cargo
 
 class Funcionario(models.Model):
-    nome_func = models.CharField(max_length=30)
-    sobrenome_func = models.CharField(max_length=50)
+    nome_func = models.CharField(max_length=30, verbose_name="nome")
+    sobrenome_func = models.CharField(max_length=50, verbose_name="sobrenome")
     GENEROS_FUNCIONARIO = (
         ('F','Feminino'),
         ('M','Masculino')
     )
-    genero_func = models.CharField(max_length=9, choices=GENEROS_FUNCIONARIO)
-    cpf_func = models.PositiveBigIntegerField()
-    nasc_func = models.DateField(auto_now=False, auto_now_add=False)
-    tel_func_1 = models.IntegerField()
-    tel_func_2 = models.IntegerField(null=True, blank=True)
-    cep_func = models.IntegerField()
-    end_func = models.CharField(max_length=300)
-    bairro_func = models.CharField(max_length=100)
-    cidade_func = models.CharField(max_length=100)
-    email_func = models.EmailField(max_length=300)
-    id_cargo = models.ForeignKey('Cargo', on_delete=models.CASCADE)
-    status_cad_func = models.BooleanField(verbose_name="Ativar Cadastro")
+    genero_func = models.CharField(max_length=9, choices=GENEROS_FUNCIONARIO, verbose_name="gênero")
+    cpf_func = models.PositiveBigIntegerField(verbose_name="CPF")
+    nasc_func = models.DateField(auto_now=False, auto_now_add=False, verbose_name="nascimento")
+    tel_func_1 = models.IntegerField(verbose_name="telefone 1")
+    tel_func_2 = models.IntegerField(null=True, blank=True, verbose_name="telefone 2 (opcional)")
+    cep_func = models.IntegerField(verbose_name="CEP")
+    end_func = models.CharField(max_length=300, verbose_name="endereço")
+    bairro_func = models.CharField(max_length=100, verbose_name="bairro")
+    cidade_func = models.CharField(max_length=100, verbose_name="cidade")
+    email_func = models.EmailField(max_length=300, verbose_name="e-mail")
+    id_cargo = models.ForeignKey('Cargo', on_delete=models.CASCADE, verbose_name="cargo")
+    status_cad_func = models.BooleanField(verbose_name="Ativar Cadastro", verbose_name="Ativar Cadastro")
 
     def __str__(self):
         return f"{self.nome_func} {self.sobrenome_func}"
 
 class Medico(models.Model):
-    id_funcionario = models.OneToOneField('Funcionario', on_delete= models.CASCADE)
-    cnpj_med = models.CharField(max_length=12, unique=True, null=True)
-    crm = models.CharField(max_length=10, unique=True)
-    id_especialidade = models.ManyToManyField('Especialidade', related_name='Medicos')
+    id_funcionario = models.OneToOneField('Funcionario', on_delete= models.CASCADE, verbose_name="funcionário")
+    cnpj_med = models.CharField(max_length=12, unique=True, null=True, verbose_name="CNPJ")
+    crm = models.CharField(max_length=10, unique=True, verbose_name="CRM")
+    id_especialidade = models.ManyToManyField('Especialidade', related_name='Medicos', verbose_name="especialidade")
 
     def __str__(self):
         nome_medico = str(self.id_funcionario.nome_func)
         return nome_medico
 
 class Especialidade(models.Model):
-    tipo_especialidade = models.CharField(max_length=30)
-    valor_consulta = models.DecimalField(max_digits=5, decimal_places=2)
+    tipo_especialidade = models.CharField(max_length=30, verbose_name="especialidade")
+    valor_consulta = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="valor")
 
     def __str__(self):
         return self.tipo_especialidade
