59bc205416591d6277a0bcecae90a96de27bdc56
YuttyAkiko <yutty.akiko@gmail.com>
2024-05-13 20:55:29 -0300

    refactor: mudei o path do template

--- vision/settings.py
+++ vision/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'vision.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'paciente', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
