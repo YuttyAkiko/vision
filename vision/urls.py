f7253e08c63f8a5c97b656b84bc4b5dd715e4c0e
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-05-14 21:16:48 -0300

    feature: adicionado pasta registration em templates; adicionado o caminho accounst nas urls

--- vision/urls.py
+++ vision/urls.py
@@ -4,6 +4,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas', include('django.contrib.auth.urls')),
     path('funcionario/', include('funcionario.urls', namespace="funcionario")),
     path('paciente/', include('paciente.urls', namespace="paciente"))
 ]
