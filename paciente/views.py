a7d8435f2ec2ad9a758fe68dce85ddc8c1d9d341
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-05-17 16:38:44 -0300

    fix: views editar consulta e cancelar consulta refatoradas; trabalhando na messages

--- paciente/views.py
+++ paciente/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect
+from django.contrib import messages
+from django.db.models import Q
 from django.urls import reverse_lazy
 from django.views.generic import View, UpdateView, DeleteView
 from .models import (
@@ -21,20 +24,32 @@ def Agendamento(request):
     return render(request, 'agendamento.html')
 
 class GeralView(View):
+    # função que irá retornar o objeto paciente
     def get(self, request, pk):
         try:
-            paciente = get_object_or_404(Paciente, pk=pk)
-            username = paciente.nome_pac
-            agendamentos = Consulta.objects.filter(id_paciente=paciente, status_cons='Agendada')
-            historicos = Consulta.objects.filter(id_paciente=paciente, status_cons='Concluída')
-            return render(request, 'dashboard/dash_paciente.html', {'paciente': paciente, 'username': username, 
-            'agendamentos': agendamentos, 'historicos': historicos})
-        
+            paciente, agendamentos, historicos = self.get_queryset(pk)
+            return render(request, 'dashboard/dash_paciente.html', {
+                'paciente': paciente,
+                'username': paciente.nome_pac,
+                'agendamentos': agendamentos,
+                'historicos': historicos
+            })
         except Paciente.DoesNotExist:
-            return render(request, '404.html')
+            return HttpResponseNotFound ("Paciente não encontrado.")
+        
+    # função que irá retornar separadamente os agendamentos e historicos pelo status da consulta
+    def get_queryset(self, paciente_id):
+        paciente = get_object_or_404(Paciente, pk=paciente_id)
+        agendamentos = Consulta.objects.filter(
+            Q(id_paciente=paciente, status_cons='Agendada') | # 'Q' adiciona mais de uma condição ao filtro
+            Q(id_paciente=paciente, status_cons='Remarcada')
+        )
+        historicos = Consulta.objects.filter(id_paciente=paciente, status_cons='Concluída')
+        return paciente, agendamentos, historicos
         
 # class AgendarConsulta():
 
+# view generica que atualiza os dados do paciente
 class AtualizarDados(UpdateView):
     model = Paciente
     form_class = Update_Paciente_Form
@@ -43,19 +58,29 @@ class AtualizarDados(UpdateView):
     def get_success_url(self):
         return reverse_lazy('paciente:geral-list', kwargs={'pk': self.get_object().id})
 
+# view generica para remarcar consulta
 class EditarConsulta(UpdateView):
     model = Consulta
     form_class = Update_Consulta_Form
     template_name = 'consulta/editar-consulta.html'
 
+    # atualiza o status da consulta para remarcada
+    def form_valid(self, form):
+        consulta = form.save(commit=False)
+        consulta.status_cons = 'Remarcada'
+        consulta.save()
+        return super().form_valid(form)
+
     def get_success_url(self):
         paciente_pk = self.object.id_paciente.pk
         return reverse_lazy('paciente:geral-list', kwargs={'pk': paciente_pk})
 
+# view generica para cancelar consulta
 class CancelarConsulta(DeleteView):
     model = Consulta
     template_name = 'consulta/cancelar-consulta.html'
 
+    # função que altera o status de agendada para cancelada
     def post(self, request, pk):
         consulta = get_object_or_404(Consulta, pk=pk)
         motivo = request.POST.get('motivo', '')
@@ -64,6 +89,8 @@ class CancelarConsulta(DeleteView):
         consulta.observacoes = motivo
         consulta.save()
 
+        messages.add_message(request, messages.SUCCESS, "Consulta cancelada com sucesso.")
+        print(messages)
         return redirect('paciente:geral-list', consulta.id_paciente.pk)
 
     
